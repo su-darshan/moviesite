dc2d8235985402521401bc766774815ead6e7702
su-darshan <sudarshan_swamy@mcafee.com>
2017-10-21 19:32:53 +0530

    Review comments incorporation
    Fixing PEP8 errors and incorporating other review comments.

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# ud036_StarterCode
+Movie Trailer Website
+---------------------
+
 Source code for a Movie Trailer website.
 Contains python code that creates a movie trailer webpage and opens it in a web browser.
+You can see 7 different favourite movie pages with an image and when you click on it,
+it will play a trailer for that movie.
+
+Prequisites:
 You need python 2.7 to run this program. 
+Internet connection to play the movie trailer.
 
 Instructions to launch this program:
 -Install python 2.7 
 -Open entertainment_center.py with Python Idle.
 -Select Run->Run Module option 
 
-
+Support:
+If you face any issues, please contact abc@yahoo.com
--- entertainment_center.py
+++ entertainment_center.py
@@ -7,43 +7,42 @@ import media
 
 toy_story = media.Movie("ToyStory",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
                      "http://www.youtube.com/watch?v=-9ceBgWV8io")
 
 panda = media.Movie("Kungfu Panda",
                     "A panda who becomes a master",
-                    "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",  # NOQA
                     "https://www.youtube.com/watch?v=PXi3Mv6KMzY")
 
 nemo = media.Movie("Find Nemo",
-                  "A father's adventure searching for his missing son",
-                  "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",
-                  "https://www.youtube.com/watch?v=wZdpNglLbt8")
+                   "A father's adventure searching for his missing son",
+                   "https://upload.wikimedia.org/wikipedia/en/2/29/Finding_Nemo.jpg",  # NOQA
+                   "https://www.youtube.com/watch?v=wZdpNglLbt8")
 
 rat = media.Movie("Ratatouille",
                   "A rat who is interested in coocking",
-                  "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                  "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",  # NOQA
                   "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
 rings = media.Movie("Lord of the Rings",
                     "The fellowship of the ring",
-                    "https://upload.wikimedia.org/wikipedia/en/thumb/8/87/Ringstrilogyposter.jpg/330px-Ringstrilogyposter.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/thumb/8/87/Ringstrilogyposter.jpg/330px-Ringstrilogyposter.jpg",  # NOQA
                     "https://www.youtube.com/watch?v=Pki6jbSbXIY")
 
 
 tomorrow = media.Movie("Tomorrow Land",
-                        "A futuristic paranormal activity",
-                       "https://upload.wikimedia.org/wikipedia/en/8/80/Tomorrowland_poster.jpg",
+                       "A futuristic paranormal activity",
+                       "https://upload.wikimedia.org/wikipedia/en/8/80/Tomorrowland_poster.jpg",  # NOQA
                        "https://www.youtube.com/watch?v=0sH0__SpV88")
 
 # create a list of all movie instances
 movies = [toy_story, avatar, panda, nemo, rat, rings, tomorrow]
 
-#Create a webpage and open it in browser
+# Create a webpage and open it in browser
 fresh_tomatoes.open_movies_page(movies)
-
